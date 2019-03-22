3b03f10134c7f07c0b46f771c3ee838d35058f33
Euan <euan_forrester@hotmail.com>
2019-03-22 16:30:56 -0700

    Add terraform to create a kubernetes cluster

---
+++ terraform/modules/kubernetes/cluster.tf
@@ -0,0 +1,14 @@
+resource "aws_eks_cluster" "kunernetes" {
+    name            = "${var.cluster_name}"
+    role_arn        = "${aws_iam_role.kubernetes-cluster.arn}"
+
+    vpc_config {
+        security_group_ids = ["${aws_security_group.kubernetes-cluster.id}"]
+        subnet_ids         = ["${aws_subnet.kubernetes.*.id}"]
+    }
+
+    depends_on = [
+        "aws_iam_role_policy_attachment.kubernetes-cluster-AmazonEKSClusterPolicy",
+        "aws_iam_role_policy_attachment.kubernetes-cluster-AmazonEKSServicePolicy",
+    ]
+}
