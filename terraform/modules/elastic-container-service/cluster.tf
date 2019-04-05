5047e10ca233ae43c5130c4004586fe6cc5deeea
Euan <euan_forrester@hotmail.com>
2019-04-05 01:28:40 -0700

    Reduce disc space for EC2 images

--- terraform/modules/elastic-container-service/cluster.tf
+++ terraform/modules/elastic-container-service/cluster.tf
@@ -11,7 +11,7 @@ resource "aws_launch_configuration" "ecs-launch-configuration" {
 
     root_block_device {
         volume_type = "standard"
-        volume_size = 100
+        volume_size = 30
         delete_on_termination = true
     }
 
