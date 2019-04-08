348d410486a19902f8e2542d7fbc5e7492647847
Euan <euan_forrester@hotmail.com>
2019-04-07 17:25:40 -0700

    Move ECR logging stuff into its own file

---
+++ terraform/modules/elastic-container-service/logging.tf
@@ -0,0 +1,56 @@
+# NOTE: See https://docs.aws.amazon.com/AmazonCloudWatch/latest/logs/encrypt-log-data-kms.html
+resource "aws_kms_key" "logs" {
+    description             = "Used to encrypt/decrypt logs"
+    key_usage               = "ENCRYPT_DECRYPT"
+    enable_key_rotation     = true
+    deletion_window_in_days = 30
+    policy              = <<POLICY
+{
+  "Version" : "2012-10-17",
+  "Id" : "key-default-1",
+  "Statement" : [ {
+      "Sid" : "Enable IAM User Permissions",
+      "Effect" : "Allow",
+      "Principal" : {
+        "AWS" : "arn:aws:iam::${data.aws_caller_identity.ecs_task_definition.account_id}:root"
+      },
+      "Action" : "kms:*",
+      "Resource" : "*"
+    },
+    {
+      "Effect": "Allow",
+      "Principal": { "Service": "logs.${var.region}.amazonaws.com" },
+      "Action": [ 
+        "kms:Encrypt*",
+        "kms:Decrypt*",
+        "kms:ReEncrypt*",
+        "kms:GenerateDataKey*",
+        "kms:Describe*"
+      ],
+      "Resource": "*"
+    }  
+  ]
+}
+    POLICY
+}
+
+resource "aws_cloudwatch_log_group" "log_group" {
+    name                = "${var.cluster_name}"
+    retention_in_days   = "${var.instances_log_retention_days}"
+    kms_key_id          = "${aws_kms_key.logs.arn}"
+}
+
+# Part of a task definition, used in task-definition.tf
+data "template_file" "log_configuration" {
+    template = <<EOF
+    "logConfiguration": {
+      "logDriver": "awslogs",
+      "options": {
+        "awslogs-datetime-format": "%Y-%m-%d %H:%M:%S.%f %z",
+        "awslogs-region": "${var.region}",
+        "awslogs-group": "${aws_cloudwatch_log_group.log_group.name}",
+        "awslogs-stream-prefix": "${var.cluster_name}"
+      }
+    }
+EOF
+}
--- terraform/modules/elastic-container-service/task-definition.tf
+++ terraform/modules/elastic-container-service/task-definition.tf
@@ -7,48 +7,6 @@ data "aws_caller_identity" "ecs_task_definition" {
   
 }
 
-# NOTE: See https://docs.aws.amazon.com/AmazonCloudWatch/latest/logs/encrypt-log-data-kms.html
-resource "aws_kms_key" "logs" {
-    description             = "Used to encrypt/decrypt logs"
-    key_usage               = "ENCRYPT_DECRYPT"
-    enable_key_rotation     = true
-    deletion_window_in_days = 30
-    policy              = <<POLICY
-{
-  "Version" : "2012-10-17",
-  "Id" : "key-default-1",
-  "Statement" : [ {
-      "Sid" : "Enable IAM User Permissions",
-      "Effect" : "Allow",
-      "Principal" : {
-        "AWS" : "arn:aws:iam::${data.aws_caller_identity.ecs_task_definition.account_id}:root"
-      },
-      "Action" : "kms:*",
-      "Resource" : "*"
-    },
-    {
-      "Effect": "Allow",
-      "Principal": { "Service": "logs.${var.region}.amazonaws.com" },
-      "Action": [ 
-        "kms:Encrypt*",
-        "kms:Decrypt*",
-        "kms:ReEncrypt*",
-        "kms:GenerateDataKey*",
-        "kms:Describe*"
-      ],
-      "Resource": "*"
-    }  
-  ]
-}
-    POLICY
-}
-
-resource "aws_cloudwatch_log_group" "log_group" {
-    name                = "${var.cluster_name}"
-    retention_in_days   = "${var.instances_log_retention_days}"
-    kms_key_id          = "${aws_kms_key.logs.arn}"
-}
-
 # NOTE: to debug issues with this JSON, try targetting this specific resource when running terraform. 
 # Otherwise you will get a generic and confusing error message.
 # e.g. `terraform plan -target=module.puller-flickr.elastic-container-service.aws_ecs_task_definition`. 
@@ -69,15 +27,7 @@ resource "aws_ecs_task_definition" "my_task" {
       { "name": "ENVIRONMENT", "value": "${var.environment}" },
       { "name": "AWS_DEFAULT_REGION", "value": "${var.region}" }
     ],
-    "logConfiguration": {
-      "logDriver": "awslogs",
-      "options": {
-        "awslogs-datetime-format": "%Y-%m-%d %H:%M:%S.%f %z",
-        "awslogs-region": "${var.region}",
-        "awslogs-group": "${aws_cloudwatch_log_group.log_group.name}",
-        "awslogs-stream-prefix": "${var.cluster_name}"
-      }
-    }
+    ${data.template_file.log_configuration.rendered}
   }
 ]
 DEFINITION
