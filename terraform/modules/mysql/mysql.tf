212c04b06dd37fe599249e40bd102908f0e5c4fe
Euan <euan_forrester@hotmail.com>
2019-04-13 00:33:01 -0700

    Made database be publicly accessable, with security group only allowing traffic from local machine

--- terraform/modules/mysql/mysql.tf
+++ terraform/modules/mysql/mysql.tf
@@ -45,7 +45,7 @@ resource "aws_db_instance" "mysql_database" {
     username                        = "${var.database_name}_${var.environment}"
     password                        = "${var.database_password}"
 
-    publicly_accessible             = false
+    publicly_accessible             = true
     db_subnet_group_name            = "${aws_db_subnet_group.subnet_group.name}"
     vpc_security_group_ids          = [ "${aws_security_group.rds.id}" ]
 
