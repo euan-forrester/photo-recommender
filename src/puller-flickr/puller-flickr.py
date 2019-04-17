9c7e70d3a9a22559d0998f3307e70a1c3d8d2511
Euan <euan_forrester@hotmail.com>
2019-04-17 09:09:56 -0700

    Name all the parameters the same in the config file vs the parameter store

--- src/puller-flickr/puller-flickr.py
+++ src/puller-flickr/puller-flickr.py
@@ -50,16 +50,16 @@ if not "ENVIRONMENT" in os.environ:
     config.read("config/config.ini")
     config.read("config/secrets.ini")
 
-    flickr_api_key                      = config.get(CONFIG_FILE_ENVIRONMENT, "flickr.api.key")
-    flickr_api_secret                   = config.get(CONFIG_FILE_ENVIRONMENT, "flickr.api.secret")
-    flickr_api_retries                  = config.getint(CONFIG_FILE_ENVIRONMENT, "flickr.api.retries") 
-    flickr_api_max_favorites_per_call   = config.getint(CONFIG_FILE_ENVIRONMENT, "flickr.api.favorites.maxpercall")
-    flickr_api_max_favorites_to_get     = config.getint(CONFIG_FILE_ENVIRONMENT, "flickr.api.favorites.maxtoget")
-    flickr_user_id                      = config.get(CONFIG_FILE_ENVIRONMENT, "flickr.user.id")
-    memcached_location                  = config.get(CONFIG_FILE_ENVIRONMENT, "memcached.location")
-    memcached_ttl                       = config.getint(CONFIG_FILE_ENVIRONMENT, "memcached.ttl")
-    output_queue_url                    = config.get(CONFIG_FILE_ENVIRONMENT, "output.queue.url")
-    output_queue_batch_size             = config.getint(CONFIG_FILE_ENVIRONMENT, "output.queue.batchsize")
+    flickr_user_id                      = config.get(CONFIG_FILE_ENVIRONMENT, "flickr-user-id")
+    flickr_api_key                      = config.get(CONFIG_FILE_ENVIRONMENT, "flickr-api-key")
+    flickr_api_secret                   = config.get(CONFIG_FILE_ENVIRONMENT, "flickr-api-secret")
+    flickr_api_retries                  = config.getint(CONFIG_FILE_ENVIRONMENT, "flickr-api-retries") 
+    flickr_api_max_favorites_per_call   = config.getint(CONFIG_FILE_ENVIRONMENT, "flickr-api-favorites-maxpercall")
+    flickr_api_max_favorites_to_get     = config.getint(CONFIG_FILE_ENVIRONMENT, "flickr-api-favorites-maxtoget")
+    memcached_location                  = config.get(CONFIG_FILE_ENVIRONMENT, "memcached-location")
+    memcached_ttl                       = config.getint(CONFIG_FILE_ENVIRONMENT, "memcached-ttl")
+    output_queue_url                    = config.get(CONFIG_FILE_ENVIRONMENT, "output-queue-url")
+    output_queue_batch_size             = config.getint(CONFIG_FILE_ENVIRONMENT, "output-queue-batchsize")
 
 else:
     ENVIRONMENT = os.environ.get('ENVIRONMENT')
@@ -68,16 +68,16 @@ else:
 
     ssm = boto3.client('ssm') # Region is read from the AWS_DEFAULT_REGION env var
 
-    flickr_api_key                      = ssm.get_parameter(Name='/%s/puller-flickr/flickr-api-key'                         % ENVIRONMENT)['Parameter']['Value']
-    flickr_api_secret                   = ssm.get_parameter(Name='/%s/puller-flickr/flickr-secret-key'                      % ENVIRONMENT, WithDecryption=True)['Parameter']['Value']
-    flickr_api_retries                  = int(ssm.get_parameter(Name='/%s/puller-flickr/flickr-api-retries'                 % ENVIRONMENT)['Parameter']['Value'])
-    flickr_api_max_favorites_per_call   = int(ssm.get_parameter(Name='/%s/puller-flickr/flickr-api-favorites-max-per-call'  % ENVIRONMENT)['Parameter']['Value'])
-    flickr_api_max_favorites_to_get     = int(ssm.get_parameter(Name='/%s/puller-flickr/flickr-api-favorites-max-to-get'    % ENVIRONMENT)['Parameter']['Value'])
     flickr_user_id                      = ssm.get_parameter(Name='/%s/puller-flickr/flickr-user-id'                         % ENVIRONMENT)['Parameter']['Value']
+    flickr_api_key                      = ssm.get_parameter(Name='/%s/puller-flickr/flickr-api-key'                         % ENVIRONMENT)['Parameter']['Value']
+    flickr_api_secret                   = ssm.get_parameter(Name='/%s/puller-flickr/flickr-api-secret'                      % ENVIRONMENT, WithDecryption=True)['Parameter']['Value']
+    flickr_api_retries                  = int(ssm.get_parameter(Name='/%s/puller-flickr/flickr-api-retries'                 % ENVIRONMENT)['Parameter']['Value'])
+    flickr_api_max_favorites_per_call   = int(ssm.get_parameter(Name='/%s/puller-flickr/flickr-api-favorites-maxpercall'    % ENVIRONMENT)['Parameter']['Value'])
+    flickr_api_max_favorites_to_get     = int(ssm.get_parameter(Name='/%s/puller-flickr/flickr-api-favorites-maxtoget'      % ENVIRONMENT)['Parameter']['Value'])
     memcached_location                  = ssm.get_parameter(Name='/%s/puller-flickr/memcached-location'                     % ENVIRONMENT)['Parameter']['Value']
     memcached_ttl                       = int(ssm.get_parameter(Name='/%s/puller-flickr/memcached-ttl'                      % ENVIRONMENT)['Parameter']['Value'])
-    output_queue_url                    = ssm.get_parameter(Name='/%s/puller-flickr/output_queue_url'                       % ENVIRONMENT)['Parameter']['Value']
-    output_queue_batch_size             = int(ssm.get_parameter(Name='/%s/puller-flickr/output_queue_batch_size'            % ENVIRONMENT)['Parameter']['Value'])
+    output_queue_url                    = ssm.get_parameter(Name='/%s/puller-flickr/output-queue-url'                       % ENVIRONMENT)['Parameter']['Value']
+    output_queue_batch_size             = int(ssm.get_parameter(Name='/%s/puller-flickr/output-queue-batchsize'             % ENVIRONMENT)['Parameter']['Value'])
 
 # Call Flickr to my favorites, and the favorites of the users who created them
 
@@ -103,7 +103,6 @@ for photo in my_favorites:
         my_neighbors[photo['owner']] = { 'user_id': photo['owner'] }
 
 logging.debug("Found list of neighbors: ", my_neighbors)
-
 
 # To calculate the score of each neighbour we need to know its favourites
 for neighbor_id in my_neighbors:
