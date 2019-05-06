dc21d870cf5e2c520b6620705cf9ea60c27932a7
Euan <euan_forrester@hotmail.com>
2019-05-05 18:00:55 -0700

    Block public access to our bucket

--- terraform/modules/api-server/load-balancer.tf
+++ terraform/modules/api-server/load-balancer.tf
@@ -137,3 +137,12 @@ resource "aws_s3_bucket" "load_balancer_access_logs" {
         }
     }
 }
+
+resource "aws_s3_bucket_public_access_block" "load_balancer_access_logs" {
+    bucket = "${aws_s3_bucket.load_balancer_access_logs.id}"
+
+    block_public_acls         = true
+    block_public_policy       = true
+    ignore_public_acls        = true
+    restrict_public_buckets   = true
+}
