cccefd9ddd67bdfa51fe4d42fce80191bb3a5a7f
Euan <euan_forrester@hotmail.com>
2019-05-22 17:08:04 -0700

    Fixed typo in default memcached location

--- terraform/modules/puller-flickr/parameter-store.tf
+++ terraform/modules/puller-flickr/parameter-store.tf
@@ -61,7 +61,8 @@ resource "aws_ssm_parameter" "memcached_location" {
     type        = "String"
     # Ugly syntax here for referencing a resource that may not exist. See https://github.com/hashicorp/terraform/issues/16726
     # Puts "localhost:11211" in this attribute if the memcached cluster wasn't created. The script will attempt to connect to there, fail, and continue in that case
-    value       = "${format("%s:%d", element(concat(aws_elasticache_cluster.memcached.*.cluster_address, list("localhost")), 0), element(concat(aws_elasticache_cluster.memcached.*.cluster_address, list("11211")), 0))}"
+    # Also note that all variables are internally stored as strings, so having the port as an int results in a strange error message: https://github.com/hashicorp/terraform/issues/17033
+    value       = "${format("%s:%s", element(concat(aws_elasticache_cluster.memcached.*.cluster_address, list("localhost")), 0), element(concat(aws_elasticache_cluster.memcached.*.port, list("11211")), 0))}"
 }
 
 resource "aws_ssm_parameter" "output_queue_url" {
