d315eb9e34aa4c9887413ca35bdb7aa9d7050ca6
Euan <euan_forrester@hotmail.com>
2019-08-30 14:57:34 -0700

    Proxy calls to Flickr through our API server

--- terraform/dev/variables.tf
+++ terraform/dev/variables.tf
@@ -5,6 +5,5 @@ variable "local_machine_cidr" {}
 variable "local_machine_public_key" {}
 variable "flickr_api_key" {}
 variable "flickr_secret_key" {}
-variable "flickr_user_id" {}
 variable "database_password_dev" {}
 variable "dns_address" {}
