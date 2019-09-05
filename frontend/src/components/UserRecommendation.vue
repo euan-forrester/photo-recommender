82c8b74e583da775b1e382713977c7a8c2c34968
Euan <euan_forrester@hotmail.com>
2019-09-05 15:41:29 -0700

    Add comment explaining why we get user info from the front end

--- frontend/src/components/UserRecommendation.vue
+++ frontend/src/components/UserRecommendation.vue
@@ -25,12 +25,23 @@ export default {
   },
   async mounted() {
     try {
+
+      // It would probably be better if we had all the information we needed about each potential
+      // user recommendation in our database already. That would allow us to get information about
+      // how to display them faster.
+      //
+      // However, it would increase the complexity of the application and its running time (getting
+      // data from Flickr in puller-flickr is the slowest part, and this would mean adding an extra call
+      // each time, to get the user's info as well as their faves).
+      //
+      // So, in the interest of keeping the system running as quickly as possible, let's just get the
+      // info about how to display a user in the front end instead.
+
       await this.$store.dispatch('getPersonInfo', this.userId);
 
       this.personInfo = this.$store.state.personInfo[this.userId];
     } catch (error) {
       this.encounteredError = true;
-      console.log("Encountered error trying to get user info: ", error);
     }
   },
 };
