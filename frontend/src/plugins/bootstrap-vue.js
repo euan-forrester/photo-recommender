f4287d3999a08795a3086ba716b413f7ea50360f
Euan <euan_forrester@hotmail.com>
2019-10-06 16:42:35 -0700

    Only pull in plugins from bootstrap that we actually use, to reduce our dist size

--- frontend/src/plugins/bootstrap-vue.js
+++ frontend/src/plugins/bootstrap-vue.js
@@ -1,7 +1,49 @@
 import Vue from 'vue';
 
-import BootstrapVue from 'bootstrap-vue';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
+// Reduce bundle size by pulling in only what we need from bootstrap.
+// We could further optimize this by pulling in only the actual specific elements and directives
+// that we use, but it would be more work for not a ton more gain.
+// See here for more details: https://bootstrap-vue.js.org/docs/#tree-shaking-with-module-bundlers
+//
+// Note that this makes devving a bit annoying since anytime you want to try out a new
+// type you have to add it here. So consider temporarily just using
+//
+//      import { BootstrapVue } from 'bootstrap-vue';
+//      Vue.use(BootstrapVue);
+//
+// in those situations.
 
-Vue.use(BootstrapVue);
+// Place all imports from 'bootstrap-vue' in a single import
+// statement for optimal bundle sizes
+import {
+  LayoutPlugin,
+  JumbotronPlugin,
+  FormPlugin,
+  FormInputPlugin,
+  FormGroupPlugin,
+  FormTextareaPlugin,
+  ButtonPlugin,
+  AlertPlugin,
+  ImagePlugin,
+  SpinnerPlugin,
+  LinkPlugin,
+  PopoverPlugin,
+  CollapsePlugin,
+} from 'bootstrap-vue';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap-vue/dist/bootstrap-vue.css'; // Is this file necessary? The docs say it is, but I don't see anything missing without it: https://bootstrap-vue.js.org/docs/
+
+Vue.use(LayoutPlugin);
+Vue.use(JumbotronPlugin);
+Vue.use(FormPlugin);
+Vue.use(FormInputPlugin);
+Vue.use(FormGroupPlugin);
+Vue.use(FormTextareaPlugin);
+Vue.use(ButtonPlugin);
+Vue.use(AlertPlugin);
+Vue.use(ImagePlugin);
+Vue.use(SpinnerPlugin);
+Vue.use(LinkPlugin);
+Vue.use(PopoverPlugin);
+Vue.use(CollapsePlugin);
