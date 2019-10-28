89e1a6dd7a87b0b92e93637e22431bea73d51c28
Euan <euan_forrester@hotmail.com>
2019-10-28 08:44:16 -0700

    Add default num photo/user recommendations to app config

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,8 @@
 const config = {
   minNumNeighborsForRecommendations: 3,
   minNumFavoritesForRecommendations: 5,
+  defaultNumPhotoRecommendations: 50,
+  defaultNumUserRecommendations: 2,
 };
 
 export default config;
