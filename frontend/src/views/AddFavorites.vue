d8f6e42a2e18eac08b48a02a0e70196c6c94c522
Euan <euan_forrester@hotmail.com>
2019-10-28 15:09:45 -0700

    Improve not enough faves message for unauthenticated users

--- frontend/src/views/AddFavorites.vue
+++ frontend/src/views/AddFavorites.vue
@@ -1,29 +1,64 @@
 <template>
   <div>
-    <b-alert variant="danger" :show="true">
-      You need to add more favorites before we can give you recommendations!
-    </b-alert>
+    <div v-if="!userAuthenticated">
+      <b-row align-h="center">
+        <b-col xs=12 sm=8 md=6 class="notenoughfavorites">
+          <h3>
+            Sorry, {{ userName }} doesn't have enough favorites to generate any recommendations.
+          </h3>
+        </b-col>
+      </b-row>
+      <b-row align-h="center">
+        <b-col xs=12 sm=8 md=6 class="minnumfavorites">
+          <h4>
+            They have {{ numFavorites }} favorites from {{ numNeighbors }} different users, but need at least {{ appConfig.minNumFavoritesForRecommendations }} favorites from at least {{ appConfig.minNumNeighborsForRecommendations }} different users.
+          </h4>
+        </b-col>
+      </b-row>
+      <b-row align-h="center">
+        <b-col xs=12 sm=8 md=6 class="backtowelcome">
+          <h4>
+            <router-link to="/">Try someone else</router-link>
+          </h4>
+        </b-col>
+      </b-row>
+    </div>
+    <div v-else>
+      Dude you need some recommendations
+    </div>
   </div>
 </template>
+
+<style scoped>
+.notenoughfavorites {
+
+}
+
+.minnumfavorites {
+  margin-top: 30px;
+}
+
+.backtowelcome {
+  margin-top: 30px;
+}
+</style>
 
 <script>
 
 export default {
   data() {
     return {
-      userId: '',
+      userName: '',
       userAuthenticated: false,
+      numFavorites: 0,
+      numNeighbors: 0,
     };
   },
   async mounted() {
-    // console.log('Hello from add favorites');
-    // this.userId = this.$route.params.userId;
-    // this.userAuthenticated = (this.$store.getters.isAuthenticated());
-    // console.log('Finished mounting add favorites');
+    this.userName = this.$store.state.welcome.user.name;
+    this.userAuthenticated = this.$store.getters.isAuthenticated();
+    this.numFavorites = this.$store.state.welcome.user.numFavorites;
+    this.numNeighbors = this.$store.state.welcome.user.numNeighbors;
   },
 };
 </script>
-
-<style scoped>
-
-</style>
