d114501b052f99eab55aeeaa210c6caa9b12cc42
Euan <euan_forrester@hotmail.com>
2020-05-29 12:33:17 -0700

    Fix up memcached location terraform a little

--- terraform/modules/memcached/outputs.tf
+++ terraform/modules/memcached/outputs.tf
@@ -1,9 +1,13 @@
 output "location" {
   # Ugly syntax here for referencing a resource that may not exist. See https://github.com/hashicorp/terraform/issues/16726
+  # With terraform 0.12 we could use their new short-circuiting conditional logic to simplify this expression, since we wouldn't 
+  # run into an issue anymore of having the conditional reference a resource that wasn't created. However, that would mean putting the test
+  # that determines whether the resource is created or not in two places. So this method seems more rebust even though it's harder to read.
+  #
   # Puts "localhost:11211" in this attribute if the memcached cluster wasn't created. The script will attempt to connect to there, fail, and continue in that case
-  # Also note that all variables are internally stored as strings, so having the port as an int results in a strange error message: https://github.com/hashicorp/terraform/issues/17033
+  
   value = format(
-    "%s:%s",
+    "%s:%d",
     element(
       concat(
         aws_elasticache_cluster.memcached.*.cluster_address,
@@ -12,7 +16,7 @@ output "location" {
       0,
     ),
     element(
-      concat(aws_elasticache_cluster.memcached.*.port, ["11211"]),
+      concat(aws_elasticache_cluster.memcached.*.port, [11211]),
       0,
     ),
   )
